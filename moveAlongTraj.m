894ac39efde5d1566741217b003fd2767f7e0e27
Theo Winter <twinter838@gmail.com>
2024-04-30 23:50:51 -0400

    Full Robot Dynamics along trajectory working

--- moveAlongTraj.m
+++ moveAlongTraj.m
@@ -14,18 +14,18 @@ params.dt=0.01;
 params.a0=[0,0,0,0,0,0,0]';
 params.a1=[0,0,0,0,0,0,0]';
 %RNE Params
-RNEParams.g=spatialInertialMatrix
-
-
-
-
+RNEParams.G=spatialInertialMatrix(Robot);
+RNEParams.g=Robot.Gravity;
+RNEParams.M=kinematicModel.Mlist;
+RNEParams.S=kinematicModel.S;
+RNEParams.Ftip=[0,0,0,0,0,0]';
 traj=make_trajectory("quintic",params)
 for i=1:11
 
 show(Robot,traj.q(i,:)',Visuals="on",Collisions="off",FastUpdate=true,PreservePlot=false)
-RNEParams.JointPos=traj.q(i,:)
-RNEParams.JointVel=traj.v(i,:)
-RNEParams.JointAcc=traj.a(i,:)
+RNEParams.jointPos=traj.q(i,:)
+RNEParams.jointVel=traj.v(i,:)
+RNEParams.jointAcc=traj.a(i,:)
 tau=rne(RNEParams)
 tau=tau+gravityCompensation(Robot,q);
 tauList=[tauList,tau];

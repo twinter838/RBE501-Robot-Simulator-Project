42860a05b1185c0e25e3f8e615ae1ba88c096eb5
Theo Winter <twinter838@gmail.com>
2024-04-30 22:44:36 -0400

    Trajectory movement working

--- example.m
+++ example.m
@@ -1,11 +1,14 @@
 Robot=GenerateRobot("panda.urdf",true,[0,0,-9.81]);
 %Robot=loadrobot("frankaEmikaPanda","DataFormat","column")
 %Generates a new random config of the robot every time a button is pressed
+show(Robot,Robot.homeConfiguration,Visuals="off",Collisions="off",FastUpdate=true,PreservePlot=false);
+qOld=Robot.homeConfiguration;
+drawnow
 while(true)
 showdetails(Robot)
 q=Robot.randomConfiguration
 %q=[0,pi/2,0,0,0,0,0]'
-show(Robot,q,Visuals="off",Collisions="off",FastUpdate=true,PreservePlot=false)
+%show(Robot,q,Visuals="off",Collisions="off",FastUpdate=true,PreservePlot=false)
 kinematicModel=generateKinematicModel(Robot);
 
 
@@ -16,9 +19,34 @@ qTrue=getTransform(Robot,q,"panda_link8")
                        currentPose(1,3) ...
                        currentPose(2,1) ...
                        currentPose(1:3,4)']';
-qtest=ikinPanda(currentPose,kinematicModel)'
-drawnow
-tau=gravityCompensation(Robot,q);
+%qtest=ikinPanda(currentPose,kinematicModel)'
+
+tauList=moveAlongTraj(Robot,q,qOld)
+
+
+
+%tau=gravityCompensation(Robot,q);
+% params.q0=qOld;
+% params.q1=q;
+% params.v0=[0,0,0,0,0,0,0]';
+% params.v1=[0,0,0,0,0,0,0]';
+% params.t0=0;
+% params.t1=0.1;
+% params.dt=0.01;
+% params.a0=[0,0,0,0,0,0,0]';
+% params.a1=[0,0,0,0,0,0,0]';
+% 
+% traj=make_trajectory("quintic",params)
+% for i=1:11
+% 
+% show(Robot,traj.q(i,:)',Visuals="off",Collisions="off",FastUpdate=true,PreservePlot=false)
+% 
+% tau=gravityCompensation(Robot,q);
+% drawnow
+% pause(0.01)
+% end
+qOld=q
+
 waitforbuttonpress
 end
 
---
+++ utils/make_trajectory.m
@@ -0,0 +1,48 @@
+function traj = make_trajectory(type, params)
+
+    q0 = params.q0;
+    qf = params.q1;
+    v0 = params.v0;
+    vf = params.v1;
+    t0 = params.t0;
+    tf = params.t1;
+    dt = params.dt;
+
+    if strcmp(type, 'cubic')
+        A = [0      0       t0^3    t0^2    t0      1;
+             0      0       3*t0^2  2*t0    1       0;
+             0      0       0       0       0       0;
+             0      0       tf^3    tf^2    tf      1;
+             0      0       3*tf^2  2*tf    1       0;
+             0      0       0       0       0       0;];
+        
+        a0 = 0;
+        af = 0;
+        
+    elseif strcmp(type, 'quintic')
+        A = [t0^5   t0^4    t0^3    t0^2    t0      1;
+             5*t0^4  4*t0^3  3*t0^2  2*t0   1       0;
+             20*t0^3 12*t0^2 6*t0    2      0       0;
+             tf^5   tf^4    tf^3    tf^2    tf      1;
+             5*tf^4  4*tf^3  3*tf^2  2*tf   1       0;
+             20*tf^3 12*tf^2 6*tf    2      0       0;];
+
+        a0 = params.a0;
+        af = params.a1;
+
+    B = [q0 v0 a0 qf vf af]';
+    X = pinv(A) * B;
+
+
+    t = transpose(t0:dt:tf);
+
+    q = [t.^5   t.^4    t.^3    t.^2    t       t.^0] * X;
+    v = [5*t.^4  4*t.^3  3*t.^2  2*t    t.^0    t*0] * X;
+    a = [20*t.^3 12*t.^2 6*t    2*t.^0  0*t     0*t] * X;
+
+    traj.t = t;
+    traj.q = q;
+    traj.v = v;
+    traj.a = a;
+
+end

9605fa4fa0ab2bdfbdb9ee61c6ceed6bb9d34551
Theo Winter <twinter838@gmail.com>
2024-04-30 20:06:10 -0400

    more work

--- generateKinematicModel.m
+++ generateKinematicModel.m
@@ -4,7 +4,7 @@ S=double.empty(6,0);
 for i=2 :(Robot.NumBodies-1)
 currentM=getTransform(Robot,Robot.homeConfiguration,string(Robot.BodyNames(i)))
 currentV=currentM(1:3,3);
-currentP=currentM(1:3,4);
+currentP=currentM(1:3,4)
 currentS=[currentV;screw(currentV,currentP)];
 S=[S,currentS]
 string(Robot.BodyNames(i))
